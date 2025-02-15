e38cb0ea9a93a094d7a3e9caaef31300a6c25d20
Dominik Furman <domfurman01@gmail.com>
2025-02-15 14:36:17 +0100

    first commit

---
+++ src/main/java/pl/furman/adcampaignmanager_backend/AdcampaignmanagerBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.furman.adcampaignmanager_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdcampaignmanagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdcampaignmanagerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/furman/adcampaignmanager_backend/AdcampaignmanagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.furman.adcampaignmanager_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdcampaignmanagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
