b438aa732c3f97699e392e6100dae016739ba93e
Dominik Furman <domfurman01@gmail.com>
2025-02-16 19:58:49 +0100

    endpoint for predefined list of choices

--- src/main/java/pl/furman/adcampaignmanager_backend/campaign/CampaignController.java
+++ src/main/java/pl/furman/adcampaignmanager_backend/campaign/CampaignController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -41,6 +42,19 @@ public class CampaignController {
     @GetMapping("api/find-by-id/{id}")
     public Campaign findCampaignById(@PathVariable Long id) {
         return campaignService.findById(id);
+    }
+
+    @GetMapping("api/get-cities")
+    public List<String> getCities() {
+        return List.of("Cracow", "Warsaw", "Wroclaw", "Gdansk", "Rzeszow", "Szczecin", "Poznan", "Katowice", "Lublin");
+    }
+
+    @GetMapping("api/keywords")
+    public List<String> getKeywords(@RequestParam String query) {
+        List<String> allKeywords = List.of("Marketing", "SEO", "Ads", "Google", "Facebook", "Campaign");
+        return allKeywords.stream()
+                .filter(keyword -> keyword.toLowerCase().contains(query.toLowerCase()))
+                .collect(Collectors.toList());
     }
 
 
